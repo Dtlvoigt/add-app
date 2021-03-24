767d5f8956beda3a0baa0246dfb9ab455adf31ca
Dylan Voigt <dtlvoigt@gmail.com>
2021-03-23 18:41:29 -0700

    added test to make sure callbacks work

--- calculator.js
+++ calculator.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.get('/', function(req, res){
+    res.send("hello");
+});
+
+app.listen(port, function(){
+    console.log("Server running on port 3000.");
+});
